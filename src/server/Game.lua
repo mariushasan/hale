ce342b9166a29de56e9ce3c2098900d4846300ad
laurikjk <kajakko.lauri@gmail.com>
2024-04-21 14:32:33 +0300

    Enlarge and shrink boss

--- src/server/Game.lua
+++ src/server/Game.lua
@@ -24,6 +24,7 @@ local states = {
 }
 
 -- create two teams
+local bossPlayer = nil
 local bossTeam = Instance.new("Team")
 bossTeam.TeamColor = BrickColor.new("Bright red")
 bossTeam.AutoAssignable = false
@@ -54,6 +55,41 @@ local debouncedArenaPlayers = {}
 
 local state = states.END
 
+-- Utils
+function toggleCharacterSize(character, enlarge)
+	local scaleFactor = 5 -- The scale factor for enlarging the character
+	local currentFactor = enlarge and scaleFactor or (1 / scaleFactor) -- Determine the scale factor based on the 'enlarge' parameter
+
+	-- Ensure the character and its parts are fully loaded
+	character:WaitForChild("Humanoid")
+
+	-- Scale each part of the character
+	for _, part in pairs(character:GetChildren()) do
+		if part:IsA("MeshPart") or part:IsA("Part") then
+			part.Size = part.Size * currentFactor
+			local centerOffset = part.Position - character.PrimaryPart.Position
+			centerOffset = centerOffset * currentFactor
+			part.Position = character.PrimaryPart.Position + centerOffset
+		end
+	end
+
+	-- Adjust Humanoid scale factors based on the action
+	local humanoid = character:FindFirstChildOfClass("Humanoid")
+	if humanoid then
+		if enlarge then
+			humanoid.BodyWidthScale.Value = scaleFactor
+			humanoid.BodyHeightScale.Value = scaleFactor
+			humanoid.BodyDepthScale.Value = scaleFactor
+			humanoid.HeadScale.Value = scaleFactor
+		else
+			humanoid.BodyWidthScale.Value = 1
+			humanoid.BodyHeightScale.Value = 1
+			humanoid.BodyDepthScale.Value = 1
+			humanoid.HeadScale.Value = 1
+		end
+	end
+end
+
 -- Define the Game object
 local Game = {}
 
@@ -82,31 +118,14 @@ function Game.startGame()
 		player.Team = otherTeam
 	end
 
-	local bossPlayer = playersWaiting[userIds[math.random(#userIds)]]
+	bossPlayer = playersWaiting[userIds[math.random(#userIds)]]
 	bossPlayer.Team = bossTeam
 
 	-- Enlarge the boss and add additional health
 	bossPlayer.Character.Humanoid.MaxHealth = 1000
 	bossPlayer.Character.Humanoid.Health = 1000
 
-	-- Scale the boss character to 5 times its original size
-	local scaleFactor = 5
-	local character = bossPlayer.Character
-	character:WaitForChild("Humanoid")
-	for _, part in pairs(character:GetChildren()) do
-		if part:IsA("MeshPart") or part:IsA("Part") then
-			part.Size = part.Size * scaleFactor
-			local centerOffset = part.Position - character.PrimaryPart.Position
-			centerOffset = centerOffset * scaleFactor
-			part.Position = character.PrimaryPart.Position + centerOffset
-		end
-	end
-
-	-- Adjust Humanoid scale factors for the boss character
-	character.Humanoid.BodyWidthScale.Value = scaleFactor
-	character.Humanoid.BodyHeightScale.Value = scaleFactor
-	character.Humanoid.BodyDepthScale.Value = scaleFactor
-	character.Humanoid.HeadScale.Value = scaleFactor
+	toggleCharacterSize(bossPlayer.Character, true)
 
 	-- Clear players waiting queue
 	for userId in pairs(playersWaiting) do
@@ -164,6 +183,12 @@ function Game.endGame()
 
 	-- Set the game state to end
 	state = states.END
+
+	-- Reset boss player size and health
+	bossPlayer.Character.Humanoid.MaxHealth = 100
+	bossPlayer.Character.Humanoid.Health = 100
+	toggleCharacterSize(bossPlayer.Character, false)
+	bossPlayer = nil
 
 	-- Teleport players back to the waiting room
 	teleportToWait:teleportPlayers(playersInArena)
