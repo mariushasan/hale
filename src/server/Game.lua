d0e1848e4ba15d5b7e5628fba386483e35abc9aa
laurikjk <kajakko.lauri@gmail.com>
2024-04-21 14:09:27 +0300

    Assignments to boss team and other team

--- src/server/Game.lua
+++ src/server/Game.lua
@@ -13,38 +13,34 @@ local Baseplate = game.Workspace.Baseplate
 
 -- Constants
 local MINUTE = 60
-local GAME_TIME = 3*MINUTE
+local GAME_TIME = 3 * MINUTE
 local WAITING_TIME = 15
 local DEBOUNCE = 3
 local PLAYER_THRESHOLD = 1
 local states = {
-    WAITING = "WAITING",
-    PLAYING = "PLAYING",
-    END = "END"
+	WAITING = "WAITING",
+	PLAYING = "PLAYING",
+	END = "END",
 }
 
 -- create two teams
-local redTeam = Instance.new("Team")
-redTeam.TeamColor = BrickColor.new("Bright red")
-redTeam.AutoAssignable = false
-redTeam.Name = "Red Team"
-redTeam.Parent = Teams
+local bossTeam = Instance.new("Team")
+bossTeam.TeamColor = BrickColor.new("Bright red")
+bossTeam.AutoAssignable = false
+bossTeam.Name = "Boss"
+bossTeam.Parent = Teams
 
-local blueTeam = Instance.new("Team")
-blueTeam.TeamColor = BrickColor.new("Bright blue")
-blueTeam.AutoAssignable = false
-blueTeam.Name = "Blue Team"
-blueTeam.Parent = Teams
+local otherTeam = Instance.new("Team")
+otherTeam.TeamColor = BrickColor.new("Bright blue")
+otherTeam.AutoAssignable = false
+otherTeam.Name = "Other"
+otherTeam.Parent = Teams
 
 local waitingTeam = Instance.new("Team")
 waitingTeam.TeamColor = BrickColor.new("Grey")
 waitingTeam.AutoAssignable = true
 waitingTeam.Name = "Waiting"
 waitingTeam.Parent = Teams
-
-
--- start counting the number of players on each team
-local numberRed, numberBlue = 0, 0
 
 -- Local properties
 local teleportToArena = Teleport:new(TeleportPlatform, Baseplate)
@@ -62,140 +58,140 @@ local state = states.END
 local Game = {}
 
 function Game.startGame()
-    -- Debounce players to prevent teleporting back and forth
-    for userId in pairs(playersWaiting) do
-        if not deboucedWaitingPlayers[userId] then
-            deboucedWaitingPlayers[userId] = true
-            task.delay(DEBOUNCE, function()
-                deboucedWaitingPlayers[userId] = false
-            end)
-        end
-    end
+	-- Debounce players to prevent teleporting back and forth
+	for userId in pairs(playersWaiting) do
+		if not deboucedWaitingPlayers[userId] then
+			deboucedWaitingPlayers[userId] = true
+			task.delay(DEBOUNCE, function()
+				deboucedWaitingPlayers[userId] = false
+			end)
+		end
+	end
 
-    -- Set the game state to playing
-    state = states.PLAYING
+	-- Set the game state to playing
+	state = states.PLAYING
 
-    -- Teleport players to the arena
-    teleportToArena:teleportPlayers(playersWaiting)
+	-- Teleport players to the arena
+	teleportToArena:teleportPlayers(playersWaiting)
 
-    -- Team assignment logic
-    for _, player in pairs(playersWaiting) do
-        if numberRed <= numberBlue then
-            player.Team = redTeam
-            numberRed += 1
-        else
-            player.Team = blueTeam
-            numberBlue += 1
-        end
-    end
+	-- Team assignment logic
+	local userIds = {}
 
-    -- Clear players waiting queue
-    for userId in pairs(playersWaiting) do
-        playersWaiting[userId] = nil
-    end
+	for userId, player in pairs(playersWaiting) do
+		table.insert(userIds, userId)
+		player.Team = otherTeam
+	end
 
-    -- Start the game timer
-    TimerRemoteEvent:FireAllClients(GAME_TIME)
+	local bossPlayer = playersWaiting[userIds[math.random(#userIds)]]
+	bossPlayer.Team = bossTeam
 
-    -- End the game after done
-    task.delay(GAME_TIME, function()
-        Game.endGame()
-    end)
+	-- Clear players waiting queue
+	for userId in pairs(playersWaiting) do
+		playersWaiting[userId] = nil
+	end
+
+	-- Start the game timer
+	TimerRemoteEvent:FireAllClients(GAME_TIME)
+
+	-- End the game after done
+	task.delay(GAME_TIME, function()
+		Game.endGame()
+	end)
 end
 
 function Game.endGame()
-    -- Determine outcome and inform clients
-    local redTeamScore = 0
-    local blueTeamScore = 0
-    local outcome = ""
-    for _, player in redTeam:GetPlayers() do
-        redTeamScore += Leaderboard.getStat(player, "Goals")
-    end
-    for _, player in blueTeam:GetPlayers() do
-        blueTeamScore += Leaderboard.getStat(player, "Goals")
-    end
-    if redTeamScore > blueTeamScore then
-        outcome = "Red Team"
-    elseif blueTeamScore > redTeamScore then
-        outcome = "Blue Team"
-    else
-        outcome = "Draw"
-    end
+	-- Determine outcome and inform clients
+	local redTeamScore = 0
+	local blueTeamScore = 0
+	local outcome = ""
+	for _, player in bossTeam:GetPlayers() do
+		redTeamScore += Leaderboard.getStat(player, "Goals")
+	end
+	for _, player in otherTeam:GetPlayers() do
+		blueTeamScore += Leaderboard.getStat(player, "Goals")
+	end
+	if redTeamScore > blueTeamScore then
+		outcome = "Red Team"
+	elseif blueTeamScore > redTeamScore then
+		outcome = "Blue Team"
+	else
+		outcome = "Draw"
+	end
 
-    -- Debounce players to prevent teleporting back and forth
-    for userId, player in pairs(playersInArena) do
-        local playerOutcome = ""
-        if outcome == player.Team.Name then
-            playerOutcome = "Victory"
-        elseif outcome == "Draw" then
-            playerOutcome = "Draw"
-        else
-            playerOutcome = "Defeat"
-        end
+	-- Debounce players to prevent teleporting back and forth
+	for userId, player in pairs(playersInArena) do
+		local playerOutcome = ""
+		if outcome == player.Team.Name then
+			playerOutcome = "Victory"
+		elseif outcome == "Draw" then
+			playerOutcome = "Draw"
+		else
+			playerOutcome = "Defeat"
+		end
 
-        OutComeRemoteEvent:FireClient(player, playerOutcome)
-        player.Team = waitingTeam
-        if not debouncedArenaPlayers[userId] then
-            debouncedArenaPlayers[userId] = true
-            task.delay(DEBOUNCE, function()
-                debouncedArenaPlayers[userId] = false
-            end)
-        end
-    end
+		OutComeRemoteEvent:FireClient(player, playerOutcome)
+		player.Team = waitingTeam
+		if not debouncedArenaPlayers[userId] then
+			debouncedArenaPlayers[userId] = true
+			task.delay(DEBOUNCE, function()
+				debouncedArenaPlayers[userId] = false
+			end)
+		end
+	end
 
-    -- Set the game state to end
-    state = states.END
+	-- Set the game state to end
+	state = states.END
 
-    -- Teleport players back to the waiting room
-    teleportToWait:teleportPlayers(playersInArena)
+	-- Teleport players back to the waiting room
+	teleportToWait:teleportPlayers(playersInArena)
 
-    -- Reset player stats
-    local allPlayers = game.Players:GetPlayers()
-    for _, player in pairs(allPlayers) do
-        Leaderboard.setStat(player, "Goals", 0)
-    end
+	-- Reset player stats
+	local allPlayers = game.Players:GetPlayers()
+	for _, player in pairs(allPlayers) do
+		Leaderboard.setStat(player, "Goals", 0)
+	end
 
-    -- Clear players in arena queue
-    for userId, _ in pairs(playersInArena) do
-        playersInArena[userId] = nil
-    end
+	-- Clear players in arena queue
+	for userId, _ in pairs(playersInArena) do
+		playersInArena[userId] = nil
+	end
 end
 
 -- Event listeners
 TeleportPlatform.Touched:Connect(function(hit)
-    local player = game.Players:GetPlayerFromCharacter(hit.Parent)
-    if player and not playersWaiting[player.UserId] and not deboucedWaitingPlayers[player.UserId] then
-        playersWaiting[player.UserId] = player
-    end
+	local player = game.Players:GetPlayerFromCharacter(hit.Parent)
+	if player and not playersWaiting[player.UserId] and not deboucedWaitingPlayers[player.UserId] then
+		playersWaiting[player.UserId] = player
+	end
 
-    if state == states.END then
-        local playerAmount = {}
-        for k in pairs(playersWaiting) do
-            table.insert(playerAmount, k)
-        end
+	if state == states.END then
+		local playerAmount = {}
+		for k in pairs(playersWaiting) do
+			table.insert(playerAmount, k)
+		end
 
-        if #playerAmount >= PLAYER_THRESHOLD then
-            state = states.WAITING
-            task.delay(WAITING_TIME, function()
-                Game.startGame()
-            end)
-            TimerRemoteEvent:FireAllClients(WAITING_TIME)
-        end
-    end
+		if #playerAmount >= PLAYER_THRESHOLD then
+			state = states.WAITING
+			task.delay(WAITING_TIME, function()
+				Game.startGame()
+			end)
+			TimerRemoteEvent:FireAllClients(WAITING_TIME)
+		end
+	end
 end)
 
 Baseplate.Touched:Connect(function(hit)
-    local player = game.Players:GetPlayerFromCharacter(hit.Parent)
-    if player and not playersInArena[player.UserId] and not debouncedArenaPlayers[player.UserId] then
-        playersInArena[player.UserId] = player
-    end
+	local player = game.Players:GetPlayerFromCharacter(hit.Parent)
+	if player and not playersInArena[player.UserId] and not debouncedArenaPlayers[player.UserId] then
+		playersInArena[player.UserId] = player
+	end
 end)
 
 WaitingPlatform.Touched:Connect(function(hit)
-    local player = game.Players:GetPlayerFromCharacter(hit.Parent)
-    if player and playersWaiting[player.UserId] then
-        playersWaiting[player.UserId] = nil
-    end
+	local player = game.Players:GetPlayerFromCharacter(hit.Parent)
+	if player and playersWaiting[player.UserId] then
+		playersWaiting[player.UserId] = nil
+	end
 end)
 
 return Game
